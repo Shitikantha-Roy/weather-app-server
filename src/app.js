dc31f392feb4cf0e28649d3e8e19ecd26f3934ce
rashmirekha <rashmiswain22@gmail.com>
2020-03-18 23:12:34 +0530

    changed for the heroku

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'hbs')
 app.set('views', viewsPath)
 hbs.registerPartials(partialsPath)
 
+const port = process.env.PORT || 3000
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
@@ -96,6 +97,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port.'+port)
 })
